6d1ac3080368e4cfc7c638bc994867ce2526bc2d
tornikeKheladze <xeladze.tornike@gmail.com>
2022-12-23 14:13:06 +0400

    fix:add frontend validation to all inputs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,6 +22,7 @@
         }
        
     </style>
+    <script src="{{ asset('app.js') }}"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
